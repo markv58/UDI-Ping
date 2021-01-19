46b03447595b836f810c3696a7b9e9ec32042f3c
markv58 <markvittes@gmail.com>
2021-01-19 15:21:48 -0600

    Update ping.py

--- ping.py
+++ ping.py
@@ -2,7 +2,7 @@
 """
 This is a NodeServer was created using template for Polyglot v2 written in Python2/3
 by Einstein.42 (James Milne) milne.james@gmail.com
-v1.0.13
+v1.0.14
 """
 import polyinterface
 import sys
